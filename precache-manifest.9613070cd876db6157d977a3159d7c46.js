1de3c677b854fb808b7846f1aa77766f5efa0143
patwards <patrick.edwards@biarri.com>
2020-07-23 11:20:09 -0700

    Updates

---
+++ precache-manifest.9613070cd876db6157d977a3159d7c46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92656ea83b38dd8c3dc4833b57dd15a3",
+    "url": "/master-of-me/index.html"
+  },
+  {
+    "revision": "dfe365f8431b5916d631",
+    "url": "/master-of-me/static/js/2.7a75edfe.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/master-of-me/static/js/2.7a75edfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "749d06f7073af84414b9",
+    "url": "/master-of-me/static/js/main.e860e540.chunk.js"
+  },
+  {
+    "revision": "30e6278db85ca0aee931",
+    "url": "/master-of-me/static/js/runtime-main.b1a7f3b7.js"
+  },
+  {
+    "revision": "8820e1bd1d5c4ee7b681820637013342",
+    "url": "/master-of-me/static/media/AlarmClock.8820e1bd.jpg"
+  },
+  {
+    "revision": "3ee0771aeaf728fac8eeb77f1540baf3",
+    "url": "/master-of-me/static/media/logo.3ee0771a.svg"
+  }
+]);
